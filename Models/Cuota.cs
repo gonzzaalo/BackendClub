6a2599ead45297861b49fc40876892c7b41d3c5e
gonzzaalo <gonzalxc@gmail.com>
2024-11-23 14:14:32 -0300

    inicio proyecto

---
+++ Models/Cuota.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+public class Cuota
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Nombre { get; set; } = string.Empty;
+
+    public string Descripcion { get; set; } = string.Empty;
+    public decimal Monto { get; set; } // Nuevo campo para el monto
+
+    // Relación con Deportistas (Uno a Muchos)
+    public ICollection<Deportista>? Deportistas { get; set; }
+
+    // Relación con Socios (Uno a Muchos)
+    public ICollection<Socio>? Socios { get; set; }
+}
---
+++ Models/Deporte.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+public class Deporte
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50, ErrorMessage = "El nombre no puede superar los 50 caracteres.")]
+    public string Nombre { get; set; } = string.Empty;
+
+    [StringLength(200, ErrorMessage = "La descripción no puede superar los 200 caracteres.")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    public TimeSpan Hora { get; set; }
+
+    // Relación con Profesor (Muchos a Uno)
+    public int ProfesorId { get; set; }
+    public Profesor? Profesor { get; set; }
+
+    // Relación con Deportistas (Uno a Muchos)
+    public ICollection<Deportista> Deportistas { get; set; } = new List<Deportista>();
+
+    public override string ToString()
+    {
+        return Nombre;
+    }
+}
