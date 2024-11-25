31837b0e967fcc334d4828b231a040513f259245
gonzzaalo <gonzalxc@gmail.com>
2024-11-25 18:18:59 -0300

    Mejoras en validaciones y lógica de negocio en los controladores

--- Controllers/ProfesoresController.cs
+++ Controllers/ProfesoresController.cs
@@ -47,6 +47,67 @@ namespace CLubBackend.Controllers
             return profesor;
         }
 
-        // Otros métodos (PUT, POST, DELETE) se mantienen igual...
+        // PUT: api/Profesores/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProfesor(int id, Profesor profesor)
+        {
+            if (id != profesor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(profesor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProfesorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Profesores
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Profesor>> PostProfesor(Profesor profesor)
+        {
+            _context.Profesores.Add(profesor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProfesor", new { id = profesor.Id }, profesor);
+        }
+
+        // DELETE: api/Profesores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProfesor(int id)
+        {
+            var profesor = await _context.Profesores.FindAsync(id);
+            if (profesor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Profesores.Remove(profesor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProfesorExists(int id)
+        {
+            return _context.Profesores.Any(e => e.Id == id);
+        }
     }
 }
--- Controllers/SociosController.cs
+++ Controllers/SociosController.cs
@@ -47,6 +47,67 @@ namespace CLubBackend.Controllers
             return socio;
         }
 
-        // Otros métodos (PUT, POST, DELETE) se mantienen igual...
+        // PUT: api/Socios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSocio(int id, Socio socio)
+        {
+            if (id != socio.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(socio).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SocioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Socios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Socio>> PostSocio(Socio socio)
+        {
+            _context.Socios.Add(socio);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSocio", new { id = socio.Id }, socio);
+        }
+
+        // DELETE: api/Socios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSocio(int id)
+        {
+            var socio = await _context.Socios.FindAsync(id);
+            if (socio == null)
+            {
+                return NotFound();
+            }
+
+            _context.Socios.Remove(socio);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SocioExists(int id)
+        {
+            return _context.Socios.Any(e => e.Id == id);
+        }
     }
 }
